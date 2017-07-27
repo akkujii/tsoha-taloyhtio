6cd71f604fb18ebf1fe2953b5af56239b5684eb6
Jukka Peinola <jpeinola@cs.helsinki.fi>
2017-07-27 19:29:13 +0300

    mariadb käytössä

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hei Maailma');
+var Client = require('mariasql');
+
+var c = new Client({
+	host: 'localhost',
+	user: 'testikayttaja',
+	password: 'salasana',
+	db: 'resurssit'
+});
+
+c.query('SELECT * FROM Resurssit', function(err, rows) {
+	if (err)
+		throw err;
+	console.dir(rows);
+});
+
+c.end();
