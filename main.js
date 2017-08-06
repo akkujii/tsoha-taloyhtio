5da181e5bb600f9bdf0ae2806a1d44decc5ffdf0
Jukka Peinola <jpeinola@cs.helsinki.fi>
2017-08-06 14:15:17 +0300

    Työstetty vastausten saamista www-sivuilta

--- main.js
+++ main.js
@@ -2,11 +2,37 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'pug')
 
-var toiminnot = ['Tee yksittäinen varaus', 'Varaa vakiokaika', 'Muokkaa varauksia', 
-				 'Varausten yhteenveto', 'Salasanan vaihto', 'Kirjaudu ulos']
+// var toiminnot = ['Tee yksittäinen varaus', 'Varaa vakiokaika', 'Muokkaa varauksia', 
+//				 'Varausten yhteenveto', 'Salasanan vaihto', 'Kirjaudu ulos']
+
+var toiminnot = []
+
+toiminnot.push({toiminto: 'Tee yksittäinen varaus', id: '1'})
+toiminnot.push({toiminto: 'Varaa vakioaika', id: '2'})
+toiminnot.push({toiminto: 'Muokkaa varauksia', id: '3'})
+toiminnot.push({toiminto: 'Varausten yhteenveto', id: '4'})
+toiminnot.push({toiminto: 'Salasanan vaihto', id: '5'})
+toiminnot.push({toiminto: 'Kirjaudu ulos', id: '6'})
+
+var resurssit = ['Sauna', 'Pyykkitupa']
+
+var kellonajat = ['15:00', '16:00', '17:00', '18:00']
+
 
 app.get('/', function (req, res) {
- 	res.render('paavalikko', {toiminnot})
+ 	res.render('valikko', {teksti: 'Tervetuloa', toiminnot})
+})
+
+app.get('/testi', function (req, res) {
+ 	console.log(req.body.id)
+})
+
+app.get('/resurssit', function (req, res) {
+	res.render('valikko', { teksti: 'Resurssit', toiminnot: resurssit })
+})
+
+app.get('/kellonajat', function (req, res) {
+	res.render('valikko', { teksti: 'Kellonajat', toiminnot: kellonajat})
 })
 
 app.listen(3000, function () {	
